acc3239b42c57506845f8991e966890fcd3186d6
vijay <vijaygohel5454@gmail.com>
2022-05-27 15:58:54 +0530

    fix input focus

---
+++ public/helper.js
@@ -0,0 +1,12 @@
+document.getElementById('search-input').onkeydown = function(e){
+    if(e.key == "Enter"){
+      document.getElementById('search-form').submit();
+    }
+ };
+
+ function inputFocus(e) {
+  if (e.key == "/" && e.ctrlKey) 
+      document.getElementById("search-input").focus();
+}
+
+document.onkeydown = inputFocus;
